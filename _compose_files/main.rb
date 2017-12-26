496b10bf331cd1ed65203a10bd62a52904e5b646
krongkan <yaakl2k@gmail.com>
2017-12-26 11:57:31 +0700

    firsts

--- _compose_files/main.rb
+++ _compose_files/main.rb
@@ -1,7 +1,15 @@
 compose do |c, run|
   c.service :app do |s|
     s.image_from_this_project :app
-    s.port_conf app_port: 80
     s.source_code :app
+
+    s.port_conf app_port: (run.conf :app_port)
+    s.port_conf api_port: 5000
+    s.network :api_net, aliases: ["aboss-frontend-app"] if run.conf :api_net 
+    s.network :default
+    if run.conf :app_port
+      s.environments WEBPACK_DEV_SOCKET_PORT: run.conf(:app_port)
+    end
   end
+  c.network :api_net, external: { name: "aboss_api_net_#{run.conf :api_net  }" } if run.conf :api_net
 end
